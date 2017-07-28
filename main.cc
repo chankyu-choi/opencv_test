3e49579d0d742ea4e9b8d0806973d396cd723f8f
Naver <naver@AL01254237.local>
2017-07-28 14:42:02 +0900

    remove legacy

--- main.cc
+++ main.cc
@@ -6,10 +6,7 @@ using namespace cv;
 
 int main(){
     cout << "opencv test" << endl;
-    //Mat image = imread("./output.jpg");
     Mat image = imread("./lena.jpg");
-    //Mat image(500, 500, CV_8UC1);
     cout << image.cols << "x" << image.rows << endl;
-    //imwrite("./output.jpg", image);
     return -1;
 };
